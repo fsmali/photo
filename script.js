48d6c1f58de05cdebef5b16eb5eb5f472b109dc3
ALI <fsmaccounting@hotmail.com>
2024-06-25 21:37:59 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const { idText } = require('typescript');
+
+const photos = [
+  {id: 1,name: 'nature',img: './images/1.jpeg',},
+  { id: 2, name: 'nature1', img: './images/2.jpeg' },
+  { id: 3, name: 'nature2', img: './images/3.jpeg' },
+  { id: 4, name: 'nature4', img: './images/4.jpeg' },
+  { id: 5, name: 'nature5', img: './images/5.jpeg' },
+  { id: 6, name: 'nature6', img: './images/6.jpeg' },
+  { id: 7, name: 'nature7', img: './images/7.jpeg' },
+  { id: 8, name: 'nature8', img: './images/8.jpeg' },
+  { id: 9, name: 'nature9', img: './images/9.jpeg' },
+  { id: 10, name: 'nature10', img: './images/10.jpeg' },
+  { id: 11, name: 'nature11', img: './images/11.jpeg' },
+  { id: 12, name: 'nature12', img: './images/12.jpeg' },
+  { id: 12, name: 'nature13', img: './images/13.jpeg' },
+  { id: 13, name: 'nature14', img: './images/14.jpeg' },
+  { id: 14, name: 'nature15', img: './images/15.jpeg' },
+  { id: 16, name: 'nature16', img: './images/16.jpeg' },
+];
